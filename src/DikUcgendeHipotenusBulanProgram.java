dfb5e88ca5c314ca39bb8793b1ee4e95699db84d
samet ozcelik <smtab142790@gmail.com>
2023-04-28 20:46:06 +0300

    28/04/2023

---
+++ src/DikUcgendeHipotenusBulanProgram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class DikUcgendeHipotenusBulanProgram {
+
+    public static void main(String[] args) {
+
+        int a, b;
+        Scanner input = new Scanner(System.in);
+        System.out.print("A kenar uzunluklarını giriniz : ");
+        a = input.nextInt();
+        System.out.print("B kenar uzunluklarını giriniz : ");
+        b = input.nextInt();
+        double c = Math.sqrt((a * a) + (b * b));
+        double u = (a + b + c) / 2;
+        System.out.println("C (Hipotenüs) uzunlugu = " + c);
+        System.out.println("Üçgenin çevresi = " + u * 2);
+
+    }
+}
