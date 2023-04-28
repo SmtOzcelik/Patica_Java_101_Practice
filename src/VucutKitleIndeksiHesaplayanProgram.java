5a31966ba7405a6f72b75d394b9efa05edb830a3
samet ozcelik <smtab142790@gmail.com>
2023-04-28 20:50:44 +0300

    28/04/2023

---
+++ src/VucutKitleIndeksiHesaplayanProgram.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksiHesaplayanProgram {
+
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Lütfen Kilonuzu Kilogram Cinsinden Giriniz: ");
+        double kilo= scan.nextDouble();
+        System.out.print("Lütfen Boyunuzu Metre Cinsinden Giriniz: ");
+        double boy= scan.nextDouble();
+        double vKitleInd= kilo/(boy*boy);
+        System.out.println("Vücut Kitle İndeksiniz: "+vKitleInd);
+    }
+}
