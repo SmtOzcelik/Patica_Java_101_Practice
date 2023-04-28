fb412cb48cef9aec40262f110b53084979dc799c
samet ozcelik <smtab142790@gmail.com>
2023-04-28 23:07:59 +0300

    28/04/2023

---
+++ src/YildizlarIleUcgenYapimi.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class YildizlarIleUcgenYapimi {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Pozitif bir tam sayi giriniz: ");
+        int sayi = input.nextInt();
+
+        for (int i = 1; i <= sayi; i++) {
+            for (int j = sayi; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        for (int a = 1; a <= sayi; a++) {
+            for (int c = 0; c < a; c++) {
+                System.out.print(" ");
+            }
+
+            for (int b = 0; b < (2 * sayi) - (2 * a + 1); b++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
