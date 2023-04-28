244c2f93ac2ba145f44d60f661cd486dcb81b204
samet ozcelik <smtab142790@gmail.com>
2023-04-28 23:23:48 +0300

    28/04/2023

---
+++ src/AsalSayilariBulanProgram.java
@@ -0,0 +1,18 @@
+public class AsalSayilariBulanProgram {
+    public static void main(String[] args) {
+
+        String primeNumber = "";
+        for (int i = 1; i <= 100; i++) {
+            int count = 0;
+            for (int j = i; j >= 1; j--) {
+                if (i % j == 0) {
+                    count ++;
+                }
+            }
+            if (count == 2) {
+                primeNumber = primeNumber + i + " ";
+            }
+        }
+        System.out.println(primeNumber);
+    }
+}
