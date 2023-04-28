7da60a1d3098a4a53db6ec4c8559c6fa1abfe0dd
samet ozcelik <smtab142790@gmail.com>
2023-04-28 21:10:30 +0300

    28/04/2023

---
+++ src/KullaniciGirisi.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class KullaniciGirisi {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String password = "1234";
+        System.out.print("Şifrenizi giriniz : ");
+        String userPassword = input.next();
+        String newUserPassword = "";
+        boolean check = true;
+        if (password.equals(userPassword))
+            System.out.println("Başarılı bir şekilde giriş yaptınız.");
+        else if (!password.equals(userPassword)) {
+            System.out.println("Girdiğiniz şifre yanlış. " +
+                    "\nŞifre sıfırlamak ister misiniz? \nŞifre sıfırlayacaksaksanız lütfen 1'e basın. " +
+                    "\nSıfırlamak istemiyorsanız 2'ye basıp çıkış yapabilirsiniz.");
+
+
+            int option = input.nextInt();
+            if (option == 1) {
+                while (check) {
+                    System.out.print("Lütfen yeni şifreinizi giriniz: ");
+                    newUserPassword = input.next();
+                    if (newUserPassword.equals(userPassword) || (newUserPassword.equals(password)))
+                        System.out.println("Şifre oluşturulamadı. Lütfen tekrar deneyiniz.");
+
+                    else {
+                        System.out.print("Şifre oluşturuldu");
+                        check = false;
+                    }
+
+                }
+            } else if (option == 2) {
+                System.out.println("Çıkış yapılıyor...");
+                System.exit(0);
+            }
+        }
+    }
+}
