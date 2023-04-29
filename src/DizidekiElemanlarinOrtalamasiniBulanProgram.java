0de5cc4a79ef72b4b6be5535919dd84ef8174c2d
samet ozcelik <smtab142790@gmail.com>
2023-04-29 11:02:22 +0300

    29/04/2023

---
+++ src/DizidekiElemanlarinOrtalamasiniBulanProgram.java
@@ -0,0 +1,12 @@
+
+public class DizidekiElemanlarinOrtalamasiniBulanProgram {
+
+    public static void main(String[] args) {
+        double[] list = {2, 3, 4, 5, 6, 7,};
+        double sum = 0;
+        for (int i = 0; i < list.length; i++) {
+            sum += list[i];
+        }
+        System.out.println(sum / list.length);
+    }
+}
